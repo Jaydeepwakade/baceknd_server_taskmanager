4ad93bff14e781cbe492dc1d20779e48614864c0
harshv0624 <harshvonmail@gmail.com>
2024-06-30 13:07:37 +0530

    ok

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app=express()
 const cors=require('cors')
 const port=4000
 app.use(cors({
-  origin: 'https://taskmanager-alpha-gray.vercel.app', // or use '*' to allow all origins
+  origin: 'https://taskmanager-yily.vercel.app', // or use '*' to allow all origins
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // This allows credentials to be sent
